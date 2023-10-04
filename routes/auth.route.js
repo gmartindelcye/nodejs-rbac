3f191d566a7e96b5c240d3d170225b1ddce9039d
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2023-10-04 09:55:15 -0600

    user registration validating input

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/user.model');
+const {body, validationResult} = require('express-validator');
+
 
 router.get('/login', async(req, res, next) => {
     res.render('login');
@@ -13,9 +15,26 @@ router.get('/register', async(req, res, next) => {
     res.render('register');
 });
 
-router.post('/register', async(req, res, next) => {
+router.post('/register', [
+    body('email').trim().isEmail().withMessage('Invalid email').normalizeEmail().toLowerCase(),
+    body('password').trim().isLength({ min: 2 }).withMessage('Password must be at least 2 characters long'),
+    body('password2').custom((value, { req }) => {  
+        if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password');
+        }
+        return true;
+    }),
+] , 
+async(req, res, next) => {
     try {
-
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            errors.array().forEach(error => {
+                req.flash('error', error.msg);
+            });
+            res.render('register', { email: req.body.email, messages: req.flash() });
+            return;
+        }
         const { email } = req.body;
         const doesExist = await User.findOne({ email });
         if (doesExist) {
@@ -25,6 +44,8 @@ router.post('/register', async(req, res, next) => {
 
         const user = new User(req.body);
         await user.save();
+        req.flash('success', `${user.email} registration success, you can login now`);
+        res.redirect('/auth/login');
         res.send(user);
     } catch (error) {
         next(error);
